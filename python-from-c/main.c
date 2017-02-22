a74a20b4004e2d1977e33b342de60a1e62d42034
Berk Ozbalci <berkozbalci@gmail.com>
2017-02-22 13:20:52 +0300

    Fix indentation

--- python-from-c/main.c
+++ python-from-c/main.c
@@ -5,29 +5,29 @@
 #include <stdio.h>
 #include "glue.h"
 
-    int main(int argc, char *argv[]) {
-        Py_Initialize();
+int main(int argc, char *argv[]) {
+    Py_Initialize();
 
-        // The following two lines add the current working directory
-        // to the environment variable `PYTHONPATH`. This allows us
-        // to import Python modules in this directory.
-        PyRun_SimpleString("import sys");
-        PyRun_SimpleString("sys.path.append(\".\")");
+    // The following two lines add the current working directory
+    // to the environment variable `PYTHONPATH`. This allows us
+    // to import Python modules in this directory.
+    PyRun_SimpleString("import sys");
+    PyRun_SimpleString("sys.path.append(\".\")");
 
-        PyInit_glue();
+    PyInit_glue();
 
-        // Tests
-        for (int i = 0; i < 10; i++)
-        {
-            increment();
-        }
-
-        int x = get();
-        printf("Incremented %d times\n", x);
-
-        printf("The binary representation of the number 42 is\n");
-        say(3);
-
-        Py_Finalize();
-        return 0;
+    // Tests
+    for (int i = 0; i < 10; i++)
+    {
+        increment();
     }
+
+    int x = get();
+    printf("Incremented %d times\n", x);
+
+    printf("The binary representation of the number 42 is\n");
+    say(3);
+
+    Py_Finalize();
+    return 0;
+}
